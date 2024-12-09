c06ba80a5ecb714d15abfecd9228c9645bc91082
kannan p <kannanpalani69@gmail.com>
2024-12-09 19:33:01 +0530

    gridchange

---
+++ script.js
@@ -0,0 +1,6 @@
+// const toggleMenu = document.querySelector('#check-box');
+// const menu = document.querySelector('.toggle-btn');
+
+// toggleMenu.addEventListener('change', () => {
+//     menu.style.display = toggleMenu.checked ? 'inline-block' : 'none';
+// });
